8d0d07b60d5ed77555d8c7ae07a306e9005f4601
Luan Santos <luankjs@gmail.com>
2020-07-05 02:26:29 -0300

    Add content-types, options page and translations base structure

---
+++ web/app/mu-plugins/localizations.php
@@ -0,0 +1,3 @@
+<?php
+
+include 'localizations/parser.php';
---
+++ web/app/mu-plugins/options-page.php
@@ -0,0 +1,11 @@
+<?php
+
+if( function_exists('acf_add_options_page') ) {
+
+  // Check https://www.advancedcustomfields.com/resources/acf_add_options_page/
+
+  // acf_add_options_page(array());
+
+  // acf_add_options_sub_page(array());
+
+}
