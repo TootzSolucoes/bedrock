b7f5951fd35c28277c506f0adfc60d793ce59205
Luan Santos <luankjs@gmail.com>
2020-07-07 00:23:57 -0300

    Add adjusts on translations parser

--- web/app/mu-plugins/localizations/parser.php
+++ web/app/mu-plugins/localizations/parser.php
@@ -6,9 +6,13 @@ add_action('after_setup_theme', function () {
 	$yaml = new Parser();
 	$translations_yml = $yaml->parse( file_get_contents( __DIR__ . '/translations.yml' ) );
 
-	foreach($translations_yml as $group => $strings) {
-		foreach($strings as $name => $string) {
-			pll_register_string($name, $string, $group);
+	if (is_iterable($translations_yml)) {
+		foreach($translations_yml as $group => $strings) {
+			if (is_iterable($strings)) {
+				foreach($strings as $name => $string) {
+					pll_register_string($name, $string, $group);
+				}
+			}
 		}
 	}
 });
